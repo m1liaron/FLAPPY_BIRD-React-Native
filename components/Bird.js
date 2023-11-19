835979634b0e20f768f23656c66268058a3e3798
vlad <example@gmail.com>
2023-11-20 00:05:08 +0200

    ui/ux

--- components/Bird.js
+++ components/Bird.js
@@ -1,49 +1,50 @@
+// Bird.js
 import Matter from "matter-js";
-import React, { useState } from 'react'
-import { View, Image, ImageBackground } from 'react-native';
-import bird from '../images/дрон.gif'
-import gameOverImage from '../images/boom.gif';
+import React from 'react';
+import { ImageBackground } from 'react-native';
+import bird from '../images/дрон.gif';
 
-const Bird = props => {
-    const widthBody = props.body.bounds.max.x - props.body.bounds.min.x
-    const heightBody = props.body.bounds.max.y - props.body.bounds.min.y
+const Bird = (props) => {
+  const widthBody = props.body.bounds.max.x - props.body.bounds.min.x;
+  const heightBody = props.body.bounds.max.y - props.body.bounds.min.y;
 
-    const xBody = props.body.position.x - widthBody / 2;
-    const yBody = props.body.position.y - heightBody / 2;
+  const xBody = props.body.position.x - widthBody / 2;
+  const yBody = props.body.position.y - heightBody / 2;
 
-    const color = props.color;
+  const color = props.color;
 
-
-    return (
-        <ImageBackground
-        source={bird}
-        style={{
-            // borderWidth: 1,
-            // borderColor: color,
-            borderStyle: 'solid',
-            position: 'absolute',
-            left: xBody,
-            top: yBody,
-            width: widthBody,
-            height: heightBody,
-        }}/>
-    )
-}
+  return (
+    <ImageBackground
+      source={bird}
+      style={{
+        // borderWidth: 1,
+        // borderColor: color,
+        borderStyle: 'solid',
+        position: 'absolute',
+        left: xBody,
+        top: yBody,
+        width: widthBody,
+        height: heightBody,
+      }}
+    />
+  );
+};
 
 export default (world, color, pos, size) => {
-    const initialBird = Matter.Bodies.rectangle(
-        pos.x,
-        pos.y,
-        size.width,
-        size.height,
-        {label: 'Bird'}
-    ) 
-    Matter.World.add(world, initialBird)
+  const initialBird = Matter.Bodies.rectangle(
+    pos.x,
+    pos.y,
+    size.width,
+    size.height,
+    { label: 'Bird' }
+  );
 
-    return {
-        body: initialBird,
-        color,
-        pos,
-        renderer: <Bird/>
-    }
-}
+  Matter.World.add(world, initialBird);
+
+  return {
+    body: initialBird,
+    color,
+    pos,
+    renderer: <Bird />,
+  };
+};
